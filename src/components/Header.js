3c218a5ed31a56f0ed2cbfe9f53a254a6b23d5b6
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-12-12 11:50:23 -0600

    wip: adds handleClick function to github link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,35 @@
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../index.css';
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 
 const Header = (props)  => {
-    // const handleClick = () => {
-    //     window.open("https://github.com/chencho806")
-    // };
+    const handleClick = () => {
+        window.open("https://github.com/chencho806")
+    };
     return  (
-        <Navbar id="navbar" collapseOnSelect expand="lg"  >
+        <Navbar id="navbar" collapseOnSelect expand="lg">
       <Navbar.Brand href="home">
         Corey Alvarez
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link id="link" href="projects">Projects</Nav.Link>
-          <Nav.Link href="pricing">Pricing</Nav.Link>
-          <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <Nav.Link href="#deets">More deets</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            Dank memes
-          </Nav.Link>
+          <Link className="text-decoration-none" to="/projects">Projects</Link>
+          <Link className="text-decoration-none" to="/resume">Resume</Link>
+          <Link className="text-decoration-none" to="/" onClick={handleClick}>GitHub</Link>
+          <Link className="text-decoration-none" to="/contact">Contact</Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-
     );
 };
-
-
+      
+      
 export default Header;
+            
+          
+
         
         
 
