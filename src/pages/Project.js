837b5b85b06ab302674a93932aa1032630424e1d
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-12-12 16:00:45 -0600

    wip:adds cards to Projects.js

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,7 +1,87 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {  Card, Button, Container, Row } from 'react-bootstrap';
+
 const Projects = (props) => {
     return (
-        <h1>Projects</h1>
+        
+        <Container className="grid" >
+            <Row lg={4} className='justify-content-center'>
+            <Card style={{ width: '18rem', height: '24rem'}} className="box shadow-5" >
+                <Container className="img-container">
+                <Card.Img variant="top" src="/BoozeHound.jpg" className="image"/>
+                </ Container>
+
+                <Card.Body>
+
+                    <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        
+                        </Card.Text>
+
+                        <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem', height: '24rem' }} className="box">
+                <Container className="img-container">
+                <Card.Img variant="top" src="/me.jpeg" className="image"/>
+                </Container>
+                    
+                <Card.Body>
+
+                    <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        
+                        </Card.Text>
+
+                        <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem', height: '24rem' }} className="box">
+                <Container className="img-container">
+                <Card.Img variant="top" src="/me.jpeg" className="image"/>
+                </Container>
+
+                <Card.Body>
+
+                    <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        
+                        
+                        </Card.Text>
+
+                        <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem', height: '24rem' }} className="box">
+                <Container className="img-container">
+                <Card.Img variant="top" src="/me.jpeg" className="image"/>
+                </Container>
+
+                <Card.Body>
+
+                    <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        
+                        </Card.Text>
+
+                        <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            </Row>
+        </Container>
+        
+        
+        
     );
 };
 
 export default Projects
+
+
+
+
+
+
+
+
