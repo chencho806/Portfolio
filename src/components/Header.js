8781536e7b9eabed6dbe2b69708b2342c3bdce4a
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-12-12 19:40:05 -0600

    wip: adds state to collapse menue on click

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,28 @@ import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../index.css';
 import { Nav, Navbar } from "react-bootstrap";
+import { useState } from 'react';
 
 const Header = (props)  => {
     const handleClick = () => {
         window.open("https://github.com/chencho806")
+        
     };
+    const [expanded, setExpanded] = useState(false);
+
+
     return  (
-        <Navbar id="navbar" collapseOnSelect expand="lg">
-      <Navbar.Brand href="home">
+        <Navbar collapseOnSelect expanded={expanded} id="navbar"  expand="lg" bg="light">
+      <Navbar.Brand href="/">
         Corey Alvarez
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Toggle collapseOnSelect aria-controls="responsive-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")}/>
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Link className="text-decoration-none" to="/projects">Projects</Link>
-          <Link className="text-decoration-none" to="/resume">Resume</Link>
-          <Link className="text-decoration-none" to="/" onClick={handleClick}>GitHub</Link>
-          <Link className="text-decoration-none" to="/contact">Contact</Link>
+          <Link className="text-decoration-none" to="/projects" onClick={() => setExpanded(false)}>Projects</Link>
+          <Link className="text-decoration-none" to="/resume" onClick={() => setExpanded(false)}>Resume</Link>
+          <Link className="text-decoration-none" to="/"  onClick={handleClick}>GitHub</Link>
+          <Link className="text-decoration-none" to="/contact" onClick={() => setExpanded(false)}>Contact</Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
