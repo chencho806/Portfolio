7b3964059e92f4fb04eb1b55cea8ca3dfa023922
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-12-13 16:58:11 -0600

    wip: adds a click to download resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,23 @@
+
+
 const Resume = (props) => {
     return (
-        <h1>Resume</h1>
+        <div>
+            <a href="https://github.com/chencho806/resume/raw/main/Resume.pdf" className=" link-dark ">
+            <h4 className="mt-5">Click To Download</h4>
+            </a>
+            
+            <img src="./Resume.png" alt="" className="shadow p-3 mb-5 bg-white rounded img-fluid max-width: 100% height: auto" />
+        </div>
+        
     );
 };
 
 export default Resume
+
+
+
+
+        
+
+
