692eca07ff761876c129153e8551dbf1ab9b524f
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-12-13 16:57:10 -0600

    wip: adds content to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,50 @@
+
+
+
 const Home = (props) => {
     return (
-        <div className="image">
-            {/* <img src="/me.jpeg" id="img" class="img-fluid" alt="me" /> */}
+        
+        <div id="about">
+            <div className="mt-5">
+                <img src="/me2.jpg" className="img" alt="me" width="300"/>
+            </div>
+
+            <div className="mt-5">
+                <h1>Hello!</h1>
+            </div>
+
+            <div>
+                My name is Corey Alvarez and I'm a full-stack engineer based in Austin, Tx. I'm a musician, certified motorcycle mechanic, and sci-fi enthusiast. I'm a creative thinker with a team oriented mindset and management skills, seeking to begin my journey as a software developer. 
+            </div>
+            
+            <div className="mt-5">
+                <h1>Skills</h1>
+                <ul>
+                    <li>skill</li>
+                    <li>skill</li>
+                </ul>
+
+            </div>
         </div>
-    )
-}
+
+        
+    );
+};
 
 
 export default Home
+                
+
+                
+
+        
+        
+                        
+                        
+
+
+                    
+        
+
+
+
