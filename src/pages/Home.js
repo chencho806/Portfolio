f1c61624c6b37a137a49b216b786541e26fe8476
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-12-13 21:12:55 -0600

    wip: updates home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,29 +3,69 @@
 
 const Home = (props) => {
     return (
-        
+        <body>
         <div id="about">
             <div className="mt-5">
                 <img src="/me2.jpg" className="img" alt="me" width="300"/>
             </div>
 
-            <div className="mt-5">
-                <h1>Hello!</h1>
-            </div>
 
             <div>
+            <div className="mt-5">
+                <h4>Hello!</h4>
+            </div>
                 My name is Corey Alvarez and I'm a full-stack engineer based in Austin, Tx. I'm a musician, certified motorcycle mechanic, and sci-fi enthusiast. I'm a creative thinker with a team oriented mindset and management skills, seeking to begin my journey as a software developer. 
             </div>
+            <div className="mt-5">
+                I've recently attended General Assembly's Software Engineering Immersive Program and received a certificate of completion on December 6th, 2021. My primary programming language is Javascript, but I've gained some skill in Python as well. I'm most familiar with MERN stack, but I'm currently sharpening my skills in other frameworks and libraries.
+            </div>
+
             
             <div className="mt-5">
-                <h1>Skills</h1>
-                <ul>
-                    <li>skill</li>
-                    <li>skill</li>
-                </ul>
+                <div className="container">
+                        <div className="row" >
+                            <div className="col">
+                                <ul>
+                                    <li id="skills">Languages</li>
+                                    <li>JavaScript</li>
+                                    <li>Python</li>
+                                    <li>CSS3</li>
+                                    <li>HTML5</li>
+                                </ul>
+                            </div>
+                        
+                            <div className="col">
+                                <ul>
+                                    <li id="skills">Frameworks/Libraries</li>
+                                    <li>React</li>
+                                    <li>Express.js</li>
+                                    <li>jQuery</li>
+                                    <li>Django</li>
+                                </ul>
+                            </div>
+                            
+
+
+                            <div className="col">
+                                <ul>
+                                    <li id="skills">Other Technologies</li>
+                                    <li>Git</li>
+                                    <li>GitHub</li>
+                                    <li>Node.js</li>
+                                    <li>MongoDB</li>
+                                    <li>PostgreSQL</li>
+                                    
+                                </ul>
+                            </div>
+
+                        </div>
+                </div>
 
             </div>
-        </div>
+    </div>
+</body>
+            
+        
 
         
     );
