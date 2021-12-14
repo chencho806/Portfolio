6aa1d3d2a65354b4cd57743d6cbb372d54cf46f2
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-12-13 21:11:24 -0600

    wip: adds footer

---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom'
+import { BsGithub} from 'react-icons/bs'
+import { AiFillLinkedin } from 'react-icons/ai'
+
+
+const Footer = (props) => {
+    return (
+        <footer class="text-center text-white" id="footer">
+             <div class="container p-4 pb-0">
+                <section className="mb-2">
+                    <Link className="btn btn-floating m-1" id="icons" to={{ pathname: "https://github.com/chencho806" }} target="_blank"><BsGithub size="50px"/></Link>
+                    <Link className="btn btn-floating m-1" id="icons" to={{ pathname: "https://www.linkedin.com/in/corey-alvarez-276678214/" }} target="_blank"><AiFillLinkedin size="50px"/></Link>
+                </section>
+             </div>
+            <div className="text-center p-3 text-dark">
+                © 2021 Copyright Corey Alvarez 
+            </div>
+        </footer>
+    );
+};
+
+
+export default Footer;
+
+                    
+                    
+
+
+                 
+
+
+  
+    
+      
+        
+
+      
+
+      
+      
+    
+  
+  
+
+ 
+  
