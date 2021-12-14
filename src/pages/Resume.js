788e0d873564c1f1d2ae534eb8188d13a0c8b60f
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-12-13 21:13:37 -0600

    wip: updates download link

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,19 +1,25 @@
-
+import { BsDownload } from 'react-icons/bs'
 
 const Resume = (props) => {
     return (
-        <div>
-            <a href="https://github.com/chencho806/resume/raw/main/Resume.pdf" className=" link-dark ">
-            <h4 className="mt-5">Click To Download</h4>
-            </a>
-            
-            <img src="./Resume.png" alt="" className="shadow p-3 mb-5 bg-white rounded img-fluid max-width: 100% height: auto" />
-        </div>
-        
+        <body>
+            <div className="mb-5">
+                <div>
+                <a href="https://github.com/chencho806/resume/raw/main/Resume.pdf" className="link-dark">
+                <h4 className="mt-5"><BsDownload /></h4>
+                </a>
+                </div>
+
+                
+                <img src="./Resume.png" alt="" className="shadow p-3 mb-5 bg-white rounded img-fluid max-width: 100% height: auto" />
+            </div>
+        </body>
     );
 };
 
 export default Resume
+        
+            
 
 
 
