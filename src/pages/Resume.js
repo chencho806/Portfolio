bc418d8fceb11586e4a19d8f33fa8cbf44cc8a0e
“coreyalvarez” <coreyalvarez1989@gmail.com>
2022-01-13 19:28:50 -0600

    updates resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,13 +5,13 @@ const Resume = (props) => {
         <body>
             <div className="mb-5">
                 <div>
-                <a href="https://github.com/chencho806/CoreyCAlvarez-Resume/raw/main/CoreyC._Alvarez_Resume.docx(3).pdf" className="link-dark">
+                <a href="https://github.com/chencho806/resume/raw/main/Resume.pdf" className="link-dark">
                 <h4 className="mt-5"><BsDownload /></h4>
                 </a>
                 </div>
 
                 
-                <img src="./Corey-C-Alvarez-GA.png" alt="" className="shadow p-3 mb-5 bg-white rounded img-fluid max-width: 100% height: auto" />
+                <img src="./Resume.png" alt="" className="shadow p-3 mb-5 bg-white rounded img-fluid max-width: 100% height: auto" />
             </div>
         </body>
     );
